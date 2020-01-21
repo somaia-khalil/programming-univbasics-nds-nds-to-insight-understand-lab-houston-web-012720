9ace2afd7e40a33f0f9e22cec56914c6771211f8
Somaia Khalil <sumaya_khair@live.com>
2020-01-21 03:16:37 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,11 +1,20 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+require 'pp'
 # Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
-  # Change the code below to pretty print the nds with pp
-  nil
+ pp nds
 end
 
+
 def print_first_directors_movie_titles
+d_movies = directors_database[0][:movies]
+index = 0
+while index < d_movies.size do
+  titles = d_movies[index][:title]
+  puts titles
+  index+=1
 end
+end
+
