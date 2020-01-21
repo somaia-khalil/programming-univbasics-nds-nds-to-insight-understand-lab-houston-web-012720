01c4b61872bf7f73aeeca004ba388b646b738c13
Somaia Khalil <sumaya_khair@live.com>
2020-01-21 03:06:45 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+require 'pp'
 # Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
@@ -8,7 +9,7 @@ end
 
 
 def print_first_directors_movie_titles
-
+pp directors_database
 
 end
 
